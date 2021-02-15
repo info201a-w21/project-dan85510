dfedad0b505a5d27cd46900cc9fd090d46cbbbbf
dan85510 <dan85510@uw.edu>
2021-02-15 13:52:47 -0800

    finish chart three

---
+++ ChartThree - HospByRace.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(lintr)
+library(styler)
+library(ggplot2)
+covid_data <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vS8SzaERcKJOD_EzrtCDK1dX1zkoMochlA9iHoHg_RSw3V8bkpfk1mpw4pfL5RdtSOyx_oScsUtyXyk/pub?gid=43720681&single=true&output=csv")
+
+#creates a dataframe with total hospitalizations by ethnicity, by day
+hosp_by_day <- covid_data %>% 
+  select("Date", starts_with("Hosp")) %>% 
+  group_by(Date) %>% 
+  summarize(White = sum(Hosp_White, na.rm = T),
+            Black = sum(Hosp_Black, na.rm = T),
+            Latinx = sum(Hosp_Latinx, na.rm = T),
+            Asian = sum(Hosp_Asian, na.rm = T),
+            AIAN = sum(Hosp_AIAN, na.rm = T),
+            NHPI = sum(Hosp_NHPI, na.rm = T),
+            Multiracial = sum(Hosp_Multiracial, na.rm = T),
+            Other = sum(Hosp_Other, na.rm = T),
+            Unknown = sum(Hosp_Unknown, na.rm = T)
+            ) %>% 
+  mutate(Date = as.character(Date)) %>% 
+  mutate(Date = as.Date(Date, "%Y%m%d")) %>% 
+  #changes layout of the dataframe to make it suitable for plotting
+  gather(key = ethnicity,
+         value = Hospitalizations,
+         -Date)
+
+#area plot
+ggplot(hosp_by_day, aes(x = Date, y = Hospitalizations, fill = ethnicity)) +
+  geom_area()
